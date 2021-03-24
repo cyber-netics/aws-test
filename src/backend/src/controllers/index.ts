2cb1216401283908b9c603f5dfebca1aca4020b4
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-03-24 02:03:54 -0700

    test

--- src/backend/src/controllers/index.ts
+++ src/backend/src/controllers/index.ts
@@ -1,29 +1,16 @@
 import express from "express";
-import controller from "../db/actions";
 
 type PromiseVoid = Promise<void>;
 type Req = express.Request;
 type Res = express.Response;
-type Next = express.NextFunction;
 
 class User {
-  public static async getAllUsers(_: Req, res: Res, next: Next): PromiseVoid {
-    try {
-      const allUsers = await controller.findAllUsers();
-      res.status(200).json(allUsers);
-    } catch (err) {
-      next();
-    }
+  public static async getAllUsers(_: Req, res: Res): PromiseVoid {
+    res.send("hello world");
   }
 
-  public static async createUser(req: Req, res: Res, next: Next): PromiseVoid {
-    try {
-      await controller.createUser(req.body.userForm);
-      const allUsres = await controller.findAllUsers();
-      res.status(200).json(allUsres);
-    } catch (err) {
-      next();
-    }
+  public static async createUser(_: Req, res: Res): PromiseVoid {
+    res.send("hello world");
   }
 }
 
